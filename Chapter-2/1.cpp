02c445ed30203cd2ff82f191473405d892f64349
WajidAli-1 <154350557+WajidAli-1@users.noreply.github.com>
2025-03-01 17:25:59 +0500

    Update 1.cpp

--- Chapter-2/1.cpp
+++ Chapter-2/1.cpp
@@ -1,4 +1,4 @@
-
+// patch 1
 /*Assuming there are 7.481 gallons in a cubic foot, write a program that asks the user to
 enter a number of gallons, and then displays the equivalent in cubic feet.*/
 
